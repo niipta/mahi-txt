cea89786c7cb68b34ce2b7b075e2b5ba7dd47133
niipta <85364941+niipta@users.noreply.github.com>
2025-08-13 18:56:52 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,20 +3,21 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "25718812"))
+API_HASH = environ.get("API_HASH", "7ecc7924460c092f77af1307c8709b02)
+BOT_TOKEN = environ.get("BOT_TOKEN", "7586794624:AAF7CGINyvcDKxOXHF5aSv0GEiaKW6TNcGU")
 
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+OWNER = int(environ.get("OWNER", "5340095114"))
+CREDIT = environ.get("CREDIT", "𝐌𝐀𝐇𝐈𝐑 𝐀𝐋𝐈🌈™")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '5340095114').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '5340095114').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
